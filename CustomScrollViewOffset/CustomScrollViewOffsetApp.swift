8b7bc534e96992901ddd5219f625c5aa681f8a7e
Jonni <enterthesource@me.com>
2022-04-26 19:18:20 +0200

    Initial Commit

---
+++ CustomScrollViewOffset/CustomScrollViewOffsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomScrollViewOffsetApp.swift
+//  CustomScrollViewOffset
+//
+//  Created by Jonni Akesson on 2022-04-26.
+//
+
+import SwiftUI
+
+@main
+struct CustomScrollViewOffsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
